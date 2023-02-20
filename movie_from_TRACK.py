a4beaf53d2f7ce893e1d3dd6323d2e5fc73c54aa
Ivan Mikhailov <mikhailov.ivan.imc@gmail.com>
2023-02-20 22:17:38 +0300

    Fixed the animation creation by gif

--- movie_from_TRACK.py
+++ movie_from_TRACK.py
@@ -2,37 +2,38 @@ from scan_track import *
 import os
 
 PALLET = 'NOCSPF'
- 
+
+
 def print_ent_file(track, bonds, filename):
-    file = open(filename, 'w')    
+    file = open(filename, 'w')
     length_pallet = len(PALLET)
     for i in range(track.num_atoms):
         if 0 < track.btype[i] < length_pallet:
-            symbol = PALLET[track.btype[i]-1]  
+            symbol = PALLET[track.btype[i]-1]
         else:
             symbol = 'X'
         line = f'HETATM{i+1:5d}  {symbol}{i+1:12d}{track.x[i]:12.3f}{track.y[i]:8.3f}{track.z[i]:8.3f}\n'
-        file.write(line.format(i, symbol, track.x, track.y, track.z))  
-    for b in bonds: 
-        dx = abs(track.x[b[0]-1] - track.x[b[1]-1]) 
-        dy = abs(track.y[b[0]-1] - track.y[b[1]-1]) 
-        dz = abs(track.z[b[0]-1] - track.z[b[1]-1]) 
-        if dx < track.box.x/2 and dy < track.box.y/2 and dz < track.box.z/2:  
+        file.write(line.format(i, symbol, track.x, track.y, track.z))
+    for b in bonds:
+        dx = abs(track.x[b[0]-1] - track.x[b[1]-1])
+        dy = abs(track.y[b[0]-1] - track.y[b[1]-1])
+        dz = abs(track.z[b[0]-1] - track.z[b[1]-1])
+        if dx < track.box.x/2 and dy < track.box.y/2 and dz < track.box.z/2:
             file.write(f'CONECT{b[0]:5d}{b[1]:5d}\n')
-    file.close()    
+    file.close()
 
-# def spt_creation():   
-        
+
 if __name__ == '__main__':
-    path = '/home/imc/Serafim/Movie_test/'
+    path = 'trajectory_sample'
     track = ReadTrack(path)
     bonds = read_bonds(path)
-    list_ent=list()
+    list_ent = list()
+    os.system(command='rm *.ent')
     while track.one_step():
         line = f'picture_{track.time_step:012d}.ent'
         list_ent.append(line)
         print_ent_file(track, bonds, line)
-    script_spt = open('script.spt', 'w') 
+    script_spt = open('script.spt', 'w')
     for name_ent in list_ent:
         script_spt.write(f'load FILES {name_ent};\n')
         script_spt.write('background white  ;\n')
@@ -46,7 +47,9 @@ if __name__ == '__main__':
         script_spt.write(f'write JPG {name_ent[:-4]}.jpg;\n')
     script_spt.write('exitjmol\n')
     script_spt.close()
+    os.system(command='rm *.jpg')
     os.system(command='java -jar /usr/share/java/Jmol.jar -s script.spt')
     os.system(command='rm *.ent')
-    # os.system(command=r'ffmpeg -r 10 -i .jpg -b 300000k  -y test.avi')
-    # os.system(command='rm *.jpg')
+    # os.system(command=r'ffmpeg -r 10 -i *.jpg -b 300000k  -y test.avi') # TO DO why is it not working
+    os.system(command='convert -delay 10 *.jpg myimage.gif')
+    os.system(command='rm *.jpg')
--- scan_track.py
+++ scan_track.py
@@ -1,43 +1,47 @@
 import os
 import numpy as np
 
+
 def periodic(coord, box):
-        if abs(coord) > 0.5 * box: 
-            return coord - np.sign(coord) * box
-        return coord 
-    
+    if abs(coord) > 0.5 * box:
+        return coord - np.sign(coord) * box
+    return coord
+
+
 class Box():
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
+
     def periodic_correct(self, xb, yb, zb):
         xb = periodic(xb, self.x)
         yb = periodic(yb, self.y)
         zb = periodic(zb, self.z)
         return xb, yb, zb
-    
+
+
 class ReadTrack():
     def __init__(self, path=''):
         self.path = os.path.join(path, 'TRACK')
         self.time_step = 0
         try:
-           self.file_track = open(self.path, 'r')
+            self.file_track = open(self.path, 'r')
         except:
             raise FileNotFoundError(f'{str(self.path)} is not found')
         try:
-            title = self.file_track.readline().split() 
+            title = self.file_track.readline().split()
             self.num_atoms = int(title[1])
-            self.box = Box(float(title[3]), float(title[4]), float(title[5])) 
+            self.box = Box(float(title[3]), float(title[4]), float(title[5]))
         except:
             self.file_track.close()
-            raise FileNotFoundError(f'{str(self.path)} is not correct') 
-        self.x = np.zeros(self.num_atoms, dtype=float) 
-        self.y = np.zeros(self.num_atoms, dtype=float) 
-        self.z = np.zeros(self.num_atoms, dtype=float) 
+            raise FileNotFoundError(f'{str(self.path)} is not correct')
+        self.x = np.zeros(self.num_atoms, dtype=float)
+        self.y = np.zeros(self.num_atoms, dtype=float)
+        self.z = np.zeros(self.num_atoms, dtype=float)
         self.btype = np.zeros(self.num_atoms, dtype=int)
         self.iterator = list(range(self.num_atoms))
-          
+
     def one_step(self):
         try:
             title = self.file_track.readline().split()
@@ -47,12 +51,13 @@ class ReadTrack():
                 self.x[i] = float(record[1])
                 self.y[i] = float(record[2])
                 self.z[i] = float(record[3])
-                self.btype[i] = int(record[4])                
+                self.btype[i] = int(record[4])
             return True
         except:
             self.file_track.close()
-            return False  
-               
+            return False
+
+
 def read_bonds(path):
     path = os.path.join(path, 'BONDS')
     bonds = list()
@@ -61,17 +66,17 @@ def read_bonds(path):
         title = file_bonds.readline().split()
         num_bonds = int(title[1])
         for _ in range(num_bonds):
-            bonds.append(list(map(int, file_bonds.readline().split())))  
-        file_bonds.close()   
+            bonds.append(list(map(int, file_bonds.readline().split())))
+        file_bonds.close()
     except:
         loging = open('Warning.log', 'w')
-        loging.write('File BONDS was not read')   
-        loging.close()  
-    return bonds     
-                
+        loging.write('File BONDS was not read')
+        loging.close()
+    return bonds
+
+
 if __name__ == '__main__':
-    RT = ReadTrack('/home/imc/Serafim/Triblock_relax_na_06/') 
+    RT = ReadTrack('trajectory_sample')
     while RT.one_step():
         print(RT.time_step)
-    read_bonds('/home/imc/Serafim/Triblock_re_06/')
-    
+    read_bonds('trajectory_sample')
